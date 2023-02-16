70ba747e7f0dfe88433381f3cfeb335377f19d82
huzaifanabigh9091 <huzaifa.nabigh@gmail.com>
2023-02-17 04:12:06 +0500

    last commit

--- src/index.js
+++ src/index.js
@@ -132,4 +132,4 @@ class Root extends Component {
 
 export default Root;
 
-ReactDOM.render(<Root />, document.getElementById("808080"));
+ReactDOM.render(<Root />, document.getElementById("nomadic_listing"));
